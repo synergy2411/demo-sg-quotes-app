e75ace3c1dd39aa91f7b66006f8159ee93f92bd4
synergy2411 <synergy2411@outlook.com>
2019-02-22 13:00:53 +0530

    alert created

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,6 +1,6 @@
-import { IQuote } from './../../model/quote.model';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController, IonicPage, NavParams } from 'ionic-angular';
+import { IQuote } from './../../model/quote.model';
 
 @IonicPage()
 @Component({
@@ -11,9 +11,27 @@ export class QuotesPage {
 
   quoteGroup : { category: string, quotes: IQuote[],icon: string};
 
-  constructor(private navParams : NavParams){
+  constructor(private navParams : NavParams, 
+              private alertCtrl : AlertController){
     console.log("RECEIVED", this.navParams.data);
     this.quoteGroup = this.navParams.get('category');
+  }
+
+  onFavorite(quote : IQuote){
+    const alert = this.alertCtrl.create({
+      title : "Are you sure?",
+      message : 'Do your want to make it favorite?',
+      buttons : [{
+        text : "Yes, please go ahead",
+        handler : ()=>{
+          console.log(quote);
+        }
+      }, {
+        text : "No, I changed my mind.",
+        role : 'Cancel'
+      }]
+    })
+    alert.present();
   }
 
 }
