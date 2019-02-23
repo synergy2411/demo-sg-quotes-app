5abe4d5b6569e25aaf434cdfe8df337cc9126e87
synergy2411 <synergy2411@outlook.com>
2019-02-23 10:55:32 +0530

    showing img in App

--- src/pages/favorite/favorite.ts
+++ src/pages/favorite/favorite.ts
@@ -5,6 +5,7 @@ import { QuoteService } from './../../services/quote.service';
 import { IQuote } from './../../model/quote.model';
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, ModalController, AlertController } from 'ionic-angular';
+import { stringify } from '@angular/compiler/src/util';
 
 @IonicPage()
 @Component({
@@ -18,28 +19,57 @@ export class FavoritePage implements OnInit {
   constructor(private quoteService: QuoteService,
     private modalCtrl: ModalController,
     private camera: Camera,
-    private alertCtrl : AlertController,
-    private geolocation : Geolocation) { }
+    private alertCtrl: AlertController,
+    private geolocation: Geolocation) { }
 
-    getLocation(){
-      this.geolocation.getCurrentPosition()
-        .then(position => {
-          const alert = this.alertCtrl.create({
-            title : "My Location",
-            message : 'Latitude : ' + position.coords.latitude + "\nLongitude : "
-              + position.coords.longitude
-          });
-          alert.present();
-        })
-        .catch(err => {
-          console.log(err);
-          const alert = this.alertCtrl.create({
-            title : "Error",
-            message : 'Can"t locate the user'
-          });
-          alert.present();
-        })
+  img: string;
+  imgData: string;
+
+  takeAndShow() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
     }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      this.img = 'data:image/jpeg;base64,' + imageData;
+      this.imgData = imageData;
+
+      console.log("IMG : ", this.img)
+      console.log("IMG DATA", this.imgData)
+    }, (err) => {
+      const alert = this.alertCtrl.create({
+        title: "Error",
+        message: 'Can"t take picture'
+      });
+      alert.present();
+    });
+  }
+
+
+  getLocation() {
+    this.geolocation.getCurrentPosition()
+      .then(position => {
+        const alert = this.alertCtrl.create({
+          title: "My Location",
+          message: 'Latitude : ' + position.coords.latitude + "\nLongitude : "
+            + position.coords.longitude
+        });
+        alert.present();
+      })
+      .catch(err => {
+        console.log(err);
+        const alert = this.alertCtrl.create({
+          title: "Error",
+          message: 'Can"t locate the user'
+        });
+        alert.present();
+      })
+  }
 
 
   openCamera() {
@@ -50,11 +80,11 @@ export class FavoritePage implements OnInit {
       mediaType: this.camera.MediaType.PICTURE
     }
     this.camera.getPicture(options)
-      .then((picture)=>{
+      .then((picture) => {
         const alert = this.alertCtrl.create({
-          title : "Camera Piture",
-          message : picture,
-          buttons : ['OK']
+          title: "Camera Piture",
+          message: picture,
+          buttons: ['OK']
         });
         alert.present();
       })
